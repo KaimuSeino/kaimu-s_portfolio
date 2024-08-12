3d42bb2f28c7a45437d491a02e0a10ea7d1d48ce
KaimuSeino <skim07a19yki@icloud.com>
2024-08-12 16:11:41 +0900

    footerコンポーネントの作成

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return ( 
+        <div className="w-full flex items-center justify-center">
+            <p className="py-10 text-slate-500">
+                &copy; 2024 Kaimu Seino
+            </p>
+        </div>
+     );
+}
+ 
+export default Footer;
