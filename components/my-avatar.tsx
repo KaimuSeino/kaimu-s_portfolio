79a3e0af4c7f838972622fdd6ff37b0d8d510dd9
KaimuSeino <skim07a19yki@icloud.com>
2024-08-12 17:12:15 +0900

    アバターの変更

--- components/my-avatar.tsx
+++ components/my-avatar.tsx
@@ -3,7 +3,7 @@ import { Avatar, AvatarImage } from "@/components/ui/avatar";
 const MyAvatar = () => {
     return (
       <Avatar className="h-60 w-60 border">
-        <AvatarImage src="/kaimu_icon.JPG" />
+        <AvatarImage src="/kaimu.JPG" />
       </Avatar>
     );
 }
