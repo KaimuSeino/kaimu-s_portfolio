4a83f75b1609902b7c3f7dea6e587346bdf3394d
KaimuSeino <skim07a19yki@icloud.com>
2024-08-12 16:47:08 +0900

    widthの設定

--- components/about.tsx
+++ components/about.tsx
@@ -8,16 +8,15 @@ const About = () => {
             <div>
               <h2 className="font-semibold text-2xl">About</h2>
               <Separator />
-              <p className="mt-2 text-pretty font-medium">
+              <p className="mt-2 text-pretty font-medium w-full md:w-[600px]">
                 清野 海夢（せいの かいむ）です。<br />
                 秋田大学 理工学部 4年生です。<br />
                 大学では、電気電子工学を専攻しており、半導体とマイクロコンピュータを用いた
                 デバイス開発に関する研究を行っています。
                 アクセラレータープログラムやハッカソンなどのイベントに参加するのが好きです。<br />
-                よく友達と新規事業やスタートアップ関連の話をしています。<br />
+                新規事業やスタートアップに興味があります。<br />
               </p>
             </div>
-  
         </div>
      );
 }
